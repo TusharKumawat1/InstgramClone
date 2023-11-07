dfd3615e5ac177c86ba436438c6892e1e0c3cd2e
TusharKumawat1 <tusharkumawat9694@gmail.com>
2023-11-07 13:39:39 +0530

    animation

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -5,7 +5,9 @@ export default function Hero() {
   //geating random path
   const list = [asset2, asset3, asset4, asset5];
   const [newPath, setNewPath] = useState(list[0]);
+  const [isanimating, setIsanimating] = useState(false);
   function changePath(list: any): any {
+    setIsanimating(false)
     return list[
       list.indexOf(newPath) === 3 ? 0 : list.indexOf(newPath) + 1
     ];
@@ -13,9 +15,10 @@ export default function Hero() {
 
   //Debouncing....
   useEffect(() => {
+    setIsanimating(true)
     const myInterval = setInterval(() => {
       setNewPath(changePath(list));
-    }, 4999);
+    }, 5000);
     return () => {
       clearInterval(myInterval);
     };
@@ -27,7 +30,7 @@ export default function Hero() {
         <img
           src={newPath}
           alt="phone"
-          className={`${Styles.randomImages}`}
+          className={`${Styles.randomImages} ${isanimating ? Styles.fade : ""} `}
         />
       </div>
     </div>
