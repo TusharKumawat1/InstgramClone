4e03a4a3340b70bb25f90dfe37db6a6e921a2a66
TusharKumawat1 <tusharkumawat9694@gmail.com>
2023-11-22 14:35:08 +0530

    working..

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,6 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/dob" element={<Dob />} />
         </Routes>
       </BrowserRouter>
     </ContextApi>
