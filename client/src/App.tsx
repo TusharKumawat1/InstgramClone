e3b22fb2dd4de6066ee500c0c7f44cd181e182cc
TusharKumawat1 <tusharkumawat9694@gmail.com>
2023-11-23 14:52:23 +0530

    working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/userHome" element={<UserHome/>} />
-          <Route path="8" element={<NotFound/>} />
+          <Route path="*" element={<NotFound/>} />
         </Routes>
       </BrowserRouter>
     </ContextApi>
