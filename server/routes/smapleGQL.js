3373b204479e1b6ab7bb7067390dbdda99f06ab0
TusharKumawat1 <tusharkumawat9694@gmail.com>
2023-12-02 12:41:29 +0530

    workin...🐥

--- server/routes/smapleGQL.js
+++ server/routes/smapleGQL.js
@@ -1,5 +1,8 @@
 import { ApolloServer } from "@apollo/server";
 import ProfileInfo from "../models/ProfileInfo.js";
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
 const server = new ApolloServer({
   typeDefs: `
         type user{
@@ -8,29 +11,44 @@ const server = new ApolloServer({
             user:String
             fullname:String
         } 
+        type Error {
+            message: String!
+          }
         type profileInfos {
             _id:String
             userId:user
             bio:String
             pfp:String
         }
+        type ProfileInfoResponse {
+            data: profileInfos
+            errors: [Error]
+          }
         type Query{
-            getPfInfo(userId: String): profileInfos
+            getPfInfo(token: String): ProfileInfoResponse
         }
     `,
   resolvers: {
     Query: {
       getPfInfo: async (_, req) => {
         try {
+            const {token}=req;
             console.log(req)
-          const pfInfo = await ProfileInfo.findOne({
-            userId: req.userId,
-          }).populate({ path: "userId", select: "-password" });
-
-          console.log(pfInfo);
-          return pfInfo;
+            if (!token) {
+                throw new Error("Please provide a token");
+            }
+            const decoded=jwt.verify(token,process.env.JWT_SIGN)
+            console.log(decoded)
+            const ifValidUser=await ProfileInfo.findOne({userId:decoded._id}).populate({
+                path:"userId",
+                select:"-password"
+            })
+            if (!ifValidUser) {
+                throw new Error("wrong token");
+            }
+            return { data: ifValidUser, errors: null };
         } catch (error) {
-          console.log(error);
+            return { errors: [{ message: error.message }] };
         }
       },
     },
