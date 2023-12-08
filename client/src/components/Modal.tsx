d69c42b31c4164f09b49b1382bc94d1ef7b8cbf0
TusharKumawat1 <tusharkumawat9694@gmail.com>
2023-12-08 16:11:47 +0530

    working

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -2,13 +2,19 @@ import React, { useContext, useState } from "react";
 import Styles from "../styles/modal.module.css";
 import ClickAwayListener from 'react-click-away-listener';
 import { MyContext } from "../context/Mycontext";
+import { FileUploader } from "react-drag-drop-files";
 export default function Modal() {
     const {setIsModalOpen}=useContext(MyContext)
+    const handleFile=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        console.log(e.target.files)
+    }
+    const fileTypes = ["JPG", "PNG", "GIF"];
   return (
             <div className={Styles.overlay}>
                 <i className={`fa-solid fa-x ${Styles.closeBtn}`}></i>
              <ClickAwayListener onClickAway={()=>setIsModalOpen(false)}>
              <div className={Styles.modal}>
+             <FileUploader handleChange={handleFile} name="file" types={fileTypes} />
                     <p className={Styles.title}>
                         Create new post
                     </p>
@@ -20,7 +26,7 @@ export default function Modal() {
                     <p>Drag photos and videos here</p>
                     <div className={Styles.inputContainer}>
                         <label htmlFor="file">Select from computer</label>
-                        <input type="file" name="file" id="file" />
+                        <input type="file" name="file" id="file" onChange={handleFile}/>
                     </div>
                     </div>
                 </div>
