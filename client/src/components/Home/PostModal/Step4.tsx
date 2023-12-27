515dfe8d4103427ae1bee2464e79b5d346c847ca
TusharKumawat1 <tusharkumawat9694@gmail.com>
2023-12-27 16:24:35 +0530

    working on search location

--- client/src/components/Home/PostModal/Step4.tsx
+++ client/src/components/Home/PostModal/Step4.tsx
@@ -4,15 +4,17 @@ import step4 from "../../../styles/components/ModalCss/step4.module.css";
 import { MyContext } from "../../../context/Mycontext";
 
 export default function Step4() {
-  const { images, setPostSteps,  aspectRatio, authenticUser,appliedFilters } =
+  const { images, setPostSteps, aspectRatio, authenticUser, appliedFilters } =
     useContext(MyContext);
-    const imageContainerRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const imageContainerRefs = useRef<(HTMLDivElement | null)[]>([]);
   const imageMaskRefs = useRef<(HTMLDivElement | null)[]>([]);
   const imageRefs = useRef<(HTMLImageElement | null)[]>([]);
   const filterBtnRef = useRef<HTMLButtonElement | null>(null);
   const imageHolderRef = useRef<HTMLDivElement | null>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [caption, setCaption] = useState("")
+  const [caption, setCaption] = useState("");
+  const [availableLocations, setAvailableLocations] = useState<string[]>([]);
+  const [currentLocation, setCurrentLocation] = useState("");
   const gotoStep3 = () => {
     setPostSteps(2);
   };
@@ -48,6 +50,22 @@ export default function Step4() {
       setCurrentIndex(newIndex);
     }
   };
+  const searchLocation = async (query: string) => {
+    //todo
+    const baseUrl = "https://nominatim.openstreetmap.org/search";
+    const url = `${baseUrl}?q=${encodeURIComponent(query)}&format=json`; // Construct the API request URL
+    try {
+      const res = await fetch(url);
+      const result = await res.json();
+      const locations: string[] = result.map((location: any) => location.display_name);
+      setAvailableLocations(locations);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // useEffect(()=>{
+  //   console.log(availableLocations)
+  // },[availableLocations])
   useEffect(() => {
     if (filterBtnRef.current) {
       filterBtnRef.current.style.color = "black";
@@ -66,36 +84,36 @@ export default function Step4() {
     if (imageRefs.current.length === images.length) {
       imageRefs.current.forEach((container, index) => {
         //accessing every mask ref available
-        const maskref= imageMaskRefs.current[index]!
-        //cheacking if any filter available 
+        const maskref = imageMaskRefs.current[index]!;
+        //cheacking if any filter available
         const maskrefs = imageMaskRefs.current!;
         maskrefs.forEach((mask, index) => {
           //if filter avaailable apply it when page load
           if (mask && appliedFilters[index]) {
             mask.className = appliedFilters[index].filter;
-            mask.style.opacity=appliedFilters[index].opacity
+            mask.style.opacity = appliedFilters[index].opacity;
           }
         });
         if (container && aspectRatio) {
           if (aspectRatio === "original") {
             container.style.width = "95%";
-            maskref.style.width="95%";
+            maskref.style.width = "95%";
           } else if (aspectRatio === "1X1") {
             container.style.width = "100%";
             container.style.height = "100%";
-            maskref.style.width="100%";
-            maskref.style.height="100%";
+            maskref.style.width = "100%";
+            maskref.style.height = "100%";
           } else if (aspectRatio === "4X5") {
             container.style.width = "80%";
             container.style.height = "100%";
-            maskref.style.width="80%";
-            maskref.style.height="100%";
+            maskref.style.width = "80%";
+            maskref.style.height = "100%";
           } else if (aspectRatio === "16X9") {
             container.style.width = "100%";
             container.style.height = "60%";
-            maskref.style.width="100%";
-            maskref.style.top="20%";
-            maskref.style.height="60%";
+            maskref.style.width = "100%";
+            maskref.style.top = "20%";
+            maskref.style.height = "60%";
           }
         }
       });
@@ -116,9 +134,21 @@ export default function Step4() {
       <div className={Styles.mainContainer}>
         <div className={Styles.imageHolder} ref={imageHolderRef}>
           {images.map((image: string, index: number) => (
-            <div className={Styles.imageContainer} key={index} ref={(el) => (imageContainerRefs.current[index] = el)}>
-              <img src={image} alt={`image${index}`} className={Styles.image} ref={(el) => (imageRefs.current[index] = el)}/>
-              <div className={Styles.imageMask} ref={(el) => (imageMaskRefs.current[index] = el)}></div>
+            <div
+              className={Styles.imageContainer}
+              key={index}
+              ref={(el) => (imageContainerRefs.current[index] = el)}
+            >
+              <img
+                src={image}
+                alt={`image${index}`}
+                className={Styles.image}
+                ref={(el) => (imageRefs.current[index] = el)}
+              />
+              <div
+                className={Styles.imageMask}
+                ref={(el) => (imageMaskRefs.current[index] = el)}
+              ></div>
             </div>
           ))}
         </div>
@@ -136,15 +166,31 @@ export default function Step4() {
         )}
 
         <div className={step4.postDetailsSection}>
-            <div className={step4.user}>
-                <img src={authenticUser.pfp} alt="" className={step4.pfp} />
-                <p>{authenticUser.userId.username}</p>
-            </div>
-            <textarea name="" id="" cols={39} rows={8} placeholder="Write a caption..." className={step4.caption} maxLength={2000} value={caption} onChange={(e)=>setCaption(e.target.value)}></textarea>
-            <div className={step4.emojiContainer}>
+          <div className={step4.user}>
+            <img src={authenticUser.pfp} alt="" className={step4.pfp} />
+            <p>{authenticUser.userId.username}</p>
+          </div>
+          <textarea
+            name=""
+            id=""
+            cols={39}
+            rows={8}
+            placeholder="Write a caption..."
+            className={step4.caption}
+            maxLength={2000}
+            value={caption}
+            onChange={(e) => setCaption(e.target.value)}
+          ></textarea>
+          <div className={step4.emojiContainer}>
             <i className="fa-regular fa-face-smile"></i>
             <p>{caption.length}/2000</p>
-            </div>
+          </div>
+          <input
+            type="text"
+            placeholder="Location"
+            value={currentLocation}
+            onChange={(e) => searchLocation(e.target.value)}
+          />
         </div>
       </div>
     </div>
